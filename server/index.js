b9071da28e52d9f18099995ea2b0ecee6eb305fd
yaominguo <missgmy@yahoo.com>
2018-02-28 22:38:13 +0800

    建立模板

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const Koa = require('koa')
 const app = new Koa()
+const {
+    model
+} = require('./template/index')
 
-app.use(async (ctx,next) => {
-    ctx.body='电影预告网站Test！'
+app.use(async (ctx, next) => {
+    ctx.type = 'text/html; charset=utf-8'
+    ctx.body = model
 })
 
 app.listen(2333)
