def275acd563c16e2b81f2e159183aa620fdff5e
yaominguo <missgmy@yahoo.com>
2018-03-01 20:53:18 +0800

    整合模板引擎中间件

--- server/index.js
+++ server/index.js
@@ -1,17 +1,28 @@
 const Koa = require('koa')
 const app = new Koa()
+const views = require('koa-views')
 const {
-    htmlTpl,
-    pugTpl
-} = require('./template/index')
-const pug = require('pug')
+    resolve
+} = require('path')
+
+// 整合模板引擎当中间件使用
+app.use(views(resolve(__dirname,'./views'),{
+    extension:'pug'
+}))
 
 app.use(async (ctx, next) => {
-    ctx.type = 'text/html; charset=utf-8'
-    ctx.body = pug.render(pugTpl,{
-        you:'Yali!',
-        me:'Guo!'
+    await ctx.render('index', {
+        you: 'Yali',
+        me: 'Guo!'
     })
 })
+
+// app.use(async (ctx, next) => {
+//     ctx.type = 'text/html; charset=utf-8'
+//     ctx.body = pug.render(pugTpl,{
+//         you:'Yali!',
+//         me:'Guo!'
+//     })
+// })
 
 app.listen(2333)
