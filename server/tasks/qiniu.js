80f031a15c34a0e609ec5afebc1d9f2e8de02c92
yaominguo <missgmy@yahoo.com>
2018-03-12 17:30:56 +0800

    构建图床上传

---
+++ server/tasks/qiniu.js
@@ -0,0 +1,6 @@
+let movies = [{
+    videoSrc: 'http://vt1.doubanio.com/201803121530/c69e25c92a4e8caec7f7412f35b1a8d3/view/movie/M/302190491.mp4',
+    doubanId: '26739551',
+    poster: ' https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2504474084.jpeg',
+    cover: 'https://img1.doubanio.com/img/trailer/medium/2493603388.jpg?'
+}]
